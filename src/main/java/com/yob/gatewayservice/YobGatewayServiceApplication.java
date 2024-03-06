2e63bbe71be89726ce39748499638a4e7ab5ceff
Kirti Vardhan <kirtivardhan8@gmail.com>
2024-03-07 02:09:11 +0530

    Project Created

---
+++ src/main/java/com/yob/gatewayservice/YobGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yob.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YobGatewayServiceApplication.class, args);
+	}
+
+}
